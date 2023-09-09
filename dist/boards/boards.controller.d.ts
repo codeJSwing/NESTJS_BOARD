bfd34558f9c143dfd37127bfca4e80f4bf58b607
codeJSwing <imlogic20@gmail.com>
2023-09-09 13:22:15 +0900

    Add: createBoard in controller

--- dist/boards/boards.controller.d.ts
+++ dist/boards/boards.controller.d.ts
@@ -1,7 +1,8 @@
 import { BoardsService } from './boards.service';
-import { Board } from "./boards.model";
+import { Board } from './boards.model';
 export declare class BoardsController {
     private boardsService;
     constructor(boardsService: BoardsService);
     getAllBoard(): Board[];
+    createBoard(title: string, description: string): Board;
 }
