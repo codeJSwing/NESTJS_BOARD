23161502392ab43b28cc9eb2cfe49d395d05d292
codeJSwing <imlogic20@gmail.com>
2023-09-09 14:17:11 +0900

    Add: check validation(use pipes)

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,16 @@
-import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    UsePipes,
+    ValidationPipe,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import {Board, BoardStatus} from './boards.model';
+import { Board, BoardStatus } from './boards.model';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
@@ -13,6 +23,7 @@ export class BoardsController {
     }
 
     @Post('/')
+    @UsePipes(ValidationPipe)
     createBoard(@Body() createBoardDto: CreateBoardDto): Board {
         return this.boardsService.createBoard(createBoardDto);
     }
@@ -30,8 +41,8 @@ export class BoardsController {
     @Patch(':id/status')
     updateBoardStatus(
         @Param('id') id: string,
-        @Body('status') status: BoardStatus
+        @Body('status') status: BoardStatus,
     ) {
-        return this.boardsService.updateBoardStatus(id, status)
+        return this.boardsService.updateBoardStatus(id, status);
     }
 }
