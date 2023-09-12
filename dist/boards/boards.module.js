051379670545cce435ace6d85f3df2db6466c190
codeJSwing <imlogic20@gmail.com>
2023-09-12 14:02:24 +0900

    Add: board repository

--- dist/boards/boards.module.js
+++ dist/boards/boards.module.js
@@ -10,11 +10,14 @@ exports.BoardsModule = void 0;
 const common_1 = require("@nestjs/common");
 const boards_controller_1 = require("./boards.controller");
 const boards_service_1 = require("./boards.service");
+const typeorm_1 = require("@nestjs/typeorm");
+const board_repository_1 = require("./board.repository");
 let BoardsModule = class BoardsModule {
 };
 exports.BoardsModule = BoardsModule;
 exports.BoardsModule = BoardsModule = __decorate([
     (0, common_1.Module)({
+        imports: [typeorm_1.TypeOrmModule.forFeature([board_repository_1.BoardRepository])],
         controllers: [boards_controller_1.BoardsController],
         providers: [boards_service_1.BoardsService],
     })
